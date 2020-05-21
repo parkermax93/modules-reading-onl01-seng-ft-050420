aff18b473e484d7fe165a92fd850ad217d638478
Parker Williamson <parker.williamson@gmail.com>
2020-05-21 02:06:17 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,6 +1,8 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
+require_relative './fancy_dance.rb'
 
 class Kid 
-  extend MetaDancing
+  extend FancyDance::ClassMethods 
+  include FancyDance::InstanceMethods 
 end
